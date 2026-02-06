43c82c01b7ee4b0a8e49e517b8fc347138bae61c
chiaochiaooo <45312913+chiaochiaoo@users.noreply.github.com>
2026-02-06 11:56:58 -0500

    try

--- src/app/login/LoginForm.tsx
+++ src/app/login/LoginForm.tsx
@@ -35,7 +35,7 @@ export function LoginForm() {
       <Icon size="lg">
         <Logo />
       </Icon>
-      <Heading>umami</Heading>
+      <Heading>SAnalytics</Heading>
       <Form onSubmit={handleSubmit} error={getErrorMessage(error)}>
         <FormField
           label={formatMessage(labels.username)}
